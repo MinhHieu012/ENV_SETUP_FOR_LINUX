4007a1b09f4a5faaac443b20edb255c2c4a44345
MinhHieu012 <0967710509hieu@gmail.com>
2024-10-22 11:29:11 +0700

    Updated to fix package docker-ce-cli is not available...

--- install_env.sh
+++ install_env.sh
@@ -126,6 +126,15 @@ while true; do
         sudo apt-get install -y software-properties-common curl screen
 
         countdown_title "Cài đặt môi trường docker" || continue
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
         sudo apt-get install \
          ca-certificates \
          curl \
@@ -135,7 +144,7 @@ while true; do
         echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt-get install docker-ce docker-ce-cli containerd.io
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
         countdown_title "Cài đặt docker-compose" || continue
         sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
